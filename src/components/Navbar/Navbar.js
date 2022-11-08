9f38ba69f5608b1441f39d953560cb7587664c28
Marcelo Acevedo <miqueasacevedo2017@gmail.com>
2022-11-08 12:50:55 -0300

    getting better navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../img/check.png';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../../css/navStyles.css';
 
 const Navbar = () => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <div className='container-fluid space-around'>
+    <nav className='navbar navbar-expand-lg d-flex'>
+      <div className='container'>
         <div className='d-flex'>
           <img src={logo} alt="logo" className='img-logo' />
-          <h2 className='m-3'>Encuestas Online</h2>
-          <button type='button' className='btn btn-coin m-2'>Buscar</button>
-          <button href='' type='button' className='btn btn-coin m-2'>Ingresar</button>
+          <h2 className='m-auto'>Encuestas Online</h2>
+          <div className="dropdown m-auto">
+            <button className="btn btn-coin dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown">
+              <i className='bi bi-search'></i>
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+          <Link to='/login' className='btn btn-coin m-auto'><i className='bi bi-door-open'></i></Link>
         </div>
       </div>
     </nav>
